2f8c89ddd2834a2298abaa0aa25ac75946986f8d
Aly <d-aly@outlook.fr>
2023-07-12 11:42:24 +0200

    Delete Post.js

--- ExpressBackendG1/models/Post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-    sujet : {type: 'String'},
-    auteur : {type: 'String'},
-    description : {type: 'String'}
-})
-
-module.exports = mongoose.model('Post', postSchema);
